ecefff3adcd70616635fbaea029370a98d8accb1
jac900 <40057442+jac900@users.noreply.github.com>
2024-12-06 10:57:55 +0100

    Update script.js

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,14 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 
-
+export function GET(request) {
+  let usersPath = path.join(process.cwd(), '/tmp/spacy_csv.csv');
+  let file = fs.readFileSync(usersPath);
+  return new Response(file);
+}
 
 
 function downloadFunc() {
 	
-	let usersPath = path.join(process.cwd(), '/tmp/spacy_csv.csv');
-    let file = fs.readFileSync(usersPath);
-
 	var element = document.createElement('a');
 	element.setAttribute('href', file);
 	element.setAttribute('download', file);
