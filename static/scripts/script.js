63b0a9b95a101719b0260d5fec3a6e98dde0363b
jac900 <40057442+jac900@users.noreply.github.com>
2024-12-06 09:56:27 +0100

    Ner

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,8 +2,8 @@
 function downloadFunc() {
 
 	var element = document.createElement('a');
-	element.setAttribute('href', process.cwd() + "/tmp/spacy_csv.csv");
-	element.setAttribute('download', process.cwd() + "/tmp/spacy_csv.csv");
+	element.setAttribute('href', process.cwd() + "/spacy_csv.csv");
+	element.setAttribute('download', process.cwd() + "/spacy_csv.csv");
 	document.body.appendChild(element);
 	element.click();
 	document.body.removeChild(element);
@@ -13,8 +13,8 @@ function downloadFunc() {
 function downloadBase() {
 	
 	var element = document.createElement('a');
-	element.setAttribute('href', process.cwd() + "/tmp/base_text.csv");
-	element.setAttribute('download', process.cwd() + "/tmp/base_text.csv");
+	element.setAttribute('href', process.cwd() + "/base_text.csv");
+	element.setAttribute('download', process.cwd() + "/base_text.csv");
 	document.body.appendChild(element);
 	element.click();
 	document.body.removeChild(element);
