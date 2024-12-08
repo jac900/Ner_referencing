03cfe95f55d2e631947913e1f49f257e107f4750
jac900 <40057442+jac900@users.noreply.github.com>
2024-12-08 19:40:58 +0100

    Update NER.py

--- NER.py
+++ NER.py
@@ -260,7 +260,7 @@ def save_pred():
         print('m_input', m_input)
 		
         list = []
-        for m, o in zip(m_input.values(), out_data.values):
+        for m, o in zip(m_input.values(), out_data.values()):
             collated = m + o
             list.append(collated)
 			
